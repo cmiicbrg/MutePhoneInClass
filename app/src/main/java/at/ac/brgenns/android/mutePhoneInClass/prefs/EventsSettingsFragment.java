5219a26cd55fe191b031d06a07affae3a295915e
Christoph Stadlbauer <christoph.stadlbauer@brgenns.ac.at>
2016-06-11 17:07:17 +0200

    Ask for enabling Locationservice on 6.0

--- app/src/main/java/at/ac/brgenns/android/mutePhoneInClass/prefs/EventsSettingsFragment.java
+++ app/src/main/java/at/ac/brgenns/android/mutePhoneInClass/prefs/EventsSettingsFragment.java
@@ -84,6 +84,7 @@ public class EventsSettingsFragment extends PreferenceFragment {
                     return true;
                 }
             });
+
             root.addPreference(p);
         }
 
